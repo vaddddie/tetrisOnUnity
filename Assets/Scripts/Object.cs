c0df2d96d4677732c4feaf5c1b86ddd341d2c64a
vaddddie <v_bykovski@mail.ru>
2022-06-17 17:19:27 +0300

    Coord method added

--- Assets/Scripts/Object.cs
+++ Assets/Scripts/Object.cs
@@ -12,8 +12,8 @@ public class Object : MonoBehaviour
 
     public float Move_interval = 1f;
 
-    private bool[,] coord = new bool[20, 10];
-    private int[] position = new int[2];
+    private bool[,] coord = new bool[10, 21];
+    private int[,] position = new int[4, 2];
     private float Step = 0.4288888f;
 
     void Start()
@@ -24,15 +24,17 @@ public class Object : MonoBehaviour
         Choose_the_object_rotations = Random.Range(1, 5);
         anim.SetInteger("Choose_the_object", Choose_the_object);
         anim.SetInteger("Choose_the_object_rotations", Choose_the_object_rotations);
-        
-        position[0] = 5;
-        position[1] = 17;
 
+        for(int i = 0; i < 10; i++)
+        {
+            coord[i, 0] = true;
+        }
+        
         pos.Rotate(0, 0, -90 * (Choose_the_object_rotations - 1));
 
         Init_State_1();
 
-        // StartCoroutine(Move_down());
+        StartCoroutine(Move_down());
     }
 
     void Update()
@@ -41,23 +43,7 @@ public class Object : MonoBehaviour
         {
             pos.Rotate(0, 0, 90);
 
-            if (Choose_the_object_rotations == 1)
-            {
-                pos.position = pos.position + new Vector3(Step / 2, Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 2)
-            {
-                pos.position = pos.position + new Vector3(Step / 2, -Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 3)
-            {
-                pos.position = pos.position + new Vector3(-Step / 2, -Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 4)
-            {
-                pos.position = pos.position + new Vector3(-Step / 2, Step / 2, 0);
-
-            }
+            Rotate_State_1_minus();
             
             if (Choose_the_object_rotations != 1)
             {
@@ -65,31 +51,14 @@ public class Object : MonoBehaviour
             } else {
                 Choose_the_object_rotations = 4;
             }
-            Debug.Log(Choose_the_object_rotations);
+
         }
 
         if (Input.GetKeyDown("e"))
         {
             pos.Rotate(0, 0, -90);
 
-            if (Choose_the_object_rotations == 1)
-            {
-                pos.position = pos.position + new Vector3(-Step / 2, Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 2)
-            {
-                pos.position = pos.position + new Vector3(Step / 2, Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 3)
-            {
-                pos.position = pos.position + new Vector3(Step / 2, -Step / 2, 0);
-
-            } else if (Choose_the_object_rotations == 4)
-            {
-                pos.position = pos.position + new Vector3(-Step / 2, -Step / 2, 0);
-
-            }
-
+            Rotate_State_1_plus();
 
             if (Choose_the_object_rotations != 4)
             {
@@ -98,17 +67,31 @@ public class Object : MonoBehaviour
                 Choose_the_object_rotations = 1;
             }
             
-            Debug.Log(Choose_the_object_rotations);
         }
     }
 
     IEnumerator Move_down()
     {
-        while(true){
-            if (position[1] > 1){
-                yield return new WaitForSeconds(Move_interval);
-                pos.position = pos.position + new Vector3(0, -Step, 0);
-                position[1] -= 1;
+        bool temp = true;
+
+        yield return new WaitForSeconds(Move_interval);
+        while(temp){
+            pos.position = pos.position + new Vector3(0, -Step, 0);
+            yield return new WaitForSeconds(Move_interval);
+
+            for (int i = 0; i < 4; i++)
+            {
+                position[i, 1] -= 1;
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (coord[position[i, 0] - 1, position[i, 1] - 2] == true)
+                {
+                    Invoke("Init_State_1()", 0.1f);
+                    temp = false;
+                    break;
+                }
             }
         }
     } 
@@ -118,12 +101,63 @@ public class Object : MonoBehaviour
         pos.position = new Vector2(0.202f, 3.654f);
         if (Choose_the_object_rotations % 2 == 1)
         {
-            pos.position = new Vector2(0.202f, 3.654f);
+            pos.position = new Vector2(0.202f, 3.654f - Step / 2);
+
+            position[0, 0] = 5 + (Choose_the_object_rotations - 1);
+            position[0, 1] = 18;
+
+            position[1, 0] = 6;
+            position[1, 1] = 18;
+
+            position[2, 0] = 7 - (Choose_the_object_rotations - 1);
+            position[2, 1] = 18;
+
+            position[3, 0] = 7 - (Choose_the_object_rotations - 1);
+            position[3, 1] = 17 + (Choose_the_object_rotations - 1);
 
         } else
         {
-            pos.position = new Vector2(0.41644f, 3.439556f);
+            pos.position = new Vector2(0.41644f - Step / 2, 3.439556f);
+
+            position[0, 0] = 6;
+            position[0, 1] = 19 - (Choose_the_object_rotations - 2);
+
+            position[1, 0] = 6;
+            position[1, 1] = 18;
+
+            position[2, 0] = 6;
+            position[2, 1] = 17 + (Choose_the_object_rotations - 2);
+
+            position[3, 0] = 5 + (Choose_the_object_rotations - 2);
+            position[3, 1] = 17 + (Choose_the_object_rotations - 2);
         }
+    }
+
+
+    private void Rotate_State_1_plus()
+    {
+        position[0, 0] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) * (-1) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3);
+        position[0, 1] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) * (-1) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3) * (-1);
+
+        position[2, 0] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3) * (-1);
+        position[2, 1] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3);
+        
+        position[3, 0] += (Choose_the_object_rotations % 2) * 2 * (Choose_the_object_rotations - 2);
+        position[3, 1] += ((Choose_the_object_rotations % 2) - 1) * 2 * (Choose_the_object_rotations - 3);
+
+    }
+
+    private void Rotate_State_1_minus()
+    {
+        position[0, 0] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) * (-1) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3);
+        position[0, 1] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3);
+
+        position[2, 0] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3) * (-1);
+        position[2, 1] += (Choose_the_object_rotations % 2) * (Choose_the_object_rotations - 2) * (-1) + ((Choose_the_object_rotations % 2) - 1) * (Choose_the_object_rotations - 3) * (-1);
+        
+        position[3, 0] += ((Choose_the_object_rotations % 2) - 1) * 2 * (Choose_the_object_rotations - 3);
+        position[3, 1] += (Choose_the_object_rotations % 2) * (-2) * (Choose_the_object_rotations - 2);
+
     }
 
 }
